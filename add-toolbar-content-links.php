d52aaed61fe8660ca440a6ac89044b28ba997741
Evan Mullins <evanrm@gmail.com>
2021-07-13 20:00:24 -0400

    Update add-toolbar-content-links.php
    Code cleanup update tested WP version.

--- add-toolbar-content-links.php
+++ add-toolbar-content-links.php
@@ -1,15 +1,20 @@
 <?php
-/*
-Plugin Name: Admin Toolbar Content Links
-Plugin URI: https://circlecube.com
-Description: Add shortcut links to the admin toolbar to the index pages listing all content types. Save clicks = save time.
-Version: 1.0
-Author: Evan Mullins
-Author URI: https://circlecube.com
-Author Email: evan@circlecube.com
-License:
+/**
+ * Plugin Name:       Admin Toolbar Content Links
+ * Plugin URI:        https://github.com/circlecube/add-toolbar-content-links
+ * Description:       Add shortcut links to the admin toolbar to the index pages listing all content types. Save clicks = save time.
+ * Version:           1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Evan Mullins
+ * Author URI:        https://circlecube.com
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-  Copyright 2011 Evan Mullins (evan@circlecube.com)
+/*
+
+  Copyright 2021 Evan Mullins (evan@circlecube.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as 
@@ -27,63 +32,13 @@ License:
 */
 
 class AddToolbarContentLinks {
-
-	/*--------------------------------------------*
-	 * Constants
-	 *--------------------------------------------*/
-	const name = 'Add Toolbar Content Links';
-	const slug = 'add_toolbar_content_links';
 	
 	/**
 	 * Constructor
 	 */
 	function __construct() {
-		//register an activation hook for the plugin
-		register_activation_hook( __FILE__, array( &$this, 'install_add_toolbar_content_links' ) );
-
-		//Hook up to the init action
-		add_action( 'init', array( &$this, 'init_add_toolbar_content_links' ) );
+		//Hook up to the admin_bar_menu action
 		add_action('admin_bar_menu', array( $this, 'custom_toolbar_link'), 999); 
-	}
-  
-	/**
-	 * Runs when the plugin is activated
-	 */  
-	function install_add_toolbar_content_links() {
-		// do not generate any output here
-	}
-  
-	/**
-	 * Runs when the plugin is initialized
-	 */
-	function init_add_toolbar_content_links() {
-		// Load JavaScript and stylesheets
-		$this->register_scripts_and_styles();
-
-	
-		if ( is_admin() ) {
-			//this will run when in the WordPress admin
-		} else {
-			//this will run when on the frontend
-		}
-
-		/*
-		 * TODO: Define custom functionality for your plugin here
-		 *
-		 * For more information: 
-		 * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-		 */
-		add_action( 'your_action_here', array( &$this, 'action_callback_method_name' ) );
-		add_filter( 'your_filter_here', array( &$this, 'filter_callback_method_name' ) ); 
-		 
-	}
-
-	function action_callback_method_name() {
-		// TODO define your action method here 
-	}
-
-	function filter_callback_method_name() {
-		// TODO define your filter method here
 	}
 
 	function custom_toolbar_link($wp_admin_bar) {
@@ -254,45 +209,6 @@ class AddToolbarContentLinks {
 		//else nothing
 	}
 
-
-  
-	/**
-	 * Registers and enqueues stylesheets for the administration panel and the
-	 * public facing site.
-	 */
-	private function register_scripts_and_styles() {
-		if ( is_admin() ) {
-			$this->load_file( self::slug . '-admin-script', '/js/admin.js', true );
-			$this->load_file( self::slug . '-admin-style', '/css/admin.css' );
-		} else {
-
-		} // end if/else
-	} // end register_scripts_and_styles
-	
-	/**
-	 * Helper function for registering and enqueueing scripts and styles.
-	 *
-	 * @name	The 	ID to register with WordPress
-	 * @file_path		The path to the actual file
-	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
-	 */
-	private function load_file( $name, $file_path, $is_script = false ) {
-
-		$url = plugins_url($file_path, __FILE__);
-		$file = plugin_dir_path(__FILE__) . $file_path;
-
-		if( file_exists( $file ) ) {
-			if( $is_script ) {
-				wp_register_script( $name, $url, array('jquery') ); //depends on jquery
-				wp_enqueue_script( $name );
-			} else {
-				wp_register_style( $name, $url );
-				wp_enqueue_style( $name );
-			} // end if
-		} // end if
-
-	} // end load_file
-  
 } // end class
 new AddToolbarContentLinks();
 
